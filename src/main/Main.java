66aa24f9e040e23b696abed22e8b46a4f0c2fc20
Jiří Čech <jiri.cech@juicymo.com>
2017-11-07 08:52:39 +0100

    Initial commit

---
+++ src/main/Main.java
@@ -0,0 +1,134 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import GUI.Mapa;
+import GUI.MenuLista;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import logika.*;
+import uiText.TextoveRozhrani;
+
+/**
+ *
+ * @author xzenj02
+ */
+public class Main extends Application {
+    private TextArea centralText;
+    private IHra hra;
+
+    public void setHra(IHra hra) {
+        this.hra = hra;
+    }
+    private TextField zadejPrikazTextArea;
+
+    private Mapa mapa;
+    private MenuLista menuLista;
+
+    @Override
+    public void start(Stage primaryStage) {
+        hra = new Hra();
+
+        mapa = new Mapa(hra);
+        menuLista = new MenuLista(hra, this);
+
+        BorderPane borderPane = new BorderPane();
+
+        // Text s prubehem hry
+        centralText = new TextArea();
+        centralText.setText(hra.vratUvitani());
+        centralText.setEditable(false);
+        borderPane.setCenter(centralText);
+
+        //label s textem zadej prikaz
+        Label zadejPrikazLabel = new Label("Zadej prikaz: ");
+        zadejPrikazLabel.setFont(Font.font("Arial", FontWeight.BOLD, 14));
+
+        // text area do ktere piseme prikazy
+        zadejPrikazTextArea = new TextField("...");
+        zadejPrikazTextArea.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+
+                String vstupniPrikaz = zadejPrikazTextArea.getText();
+                String odpovedHry = hra.zpracujPrikaz(vstupniPrikaz);
+
+                centralText.appendText("\n" + vstupniPrikaz + "\n");
+                centralText.appendText("\n" + odpovedHry + "\n");
+
+                zadejPrikazTextArea.setText("");
+
+                if (hra.konecHry()) {
+                    zadejPrikazTextArea.setEditable(false);
+                    centralText.appendText(hra.vratEpilog());
+                }
+            }
+        });
+
+        //dolni lista s elementy
+        FlowPane dolniLista = new FlowPane();
+        dolniLista.setAlignment(Pos.CENTER);
+        dolniLista.getChildren().addAll(zadejPrikazLabel,zadejPrikazTextArea);
+
+        borderPane.setLeft(mapa);
+        borderPane.setBottom(dolniLista);
+        borderPane.setTop(menuLista);
+
+        Scene scene = new Scene(borderPane, 750, 450);
+        primaryStage.setTitle("Adventura");
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        zadejPrikazTextArea.requestFocus();
+    }
+
+    public TextArea getCentralText() {
+        return centralText;
+    }
+
+    public Mapa getMapa() {
+        return mapa;
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            launch(args);
+        }
+        else{
+            if (args[0].equals("-txt")) {
+                IHra hra = new Hra();
+                TextoveRozhrani textHra = new TextoveRozhrani(hra);
+                textHra.hraj();
+            }
+            else{
+                System.out.println("Neplatny parametr");
+                System.exit(1);
+            }
+        }
+    }
+
+}
