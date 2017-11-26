bed856e4a080146655857e5621d0a433d57c3979
Jiří Čech <developer@jiricech.com>
2017-11-26 16:44:03 +0100

    Refactor map

--- src/GUI/Mapa.java
+++ src/GUI/Mapa.java
@@ -8,14 +8,13 @@ package GUI;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
-import logika.Hra;
 import logika.IHra;
 import main.Main;
 import utils.Observer;
 
 public class Mapa extends AnchorPane implements Observer {
     private IHra hra;
-    ImageView pivon = new ImageView(new Image(
+    private ImageView pivon = new ImageView(new Image(
             Main.class.getResourceAsStream("/zdroje/pivo.png"),
             30,30,true,false));
 
@@ -34,7 +33,7 @@ public class Mapa extends AnchorPane implements Observer {
 
     @Override
     public void update() {
-        this.setTopAnchor(pivon, hra.getHerniPlan().getAktualniProstor().getPosX());
-        this.setLeftAnchor(pivon, hra.getHerniPlan().getAktualniProstor().getPosY());
+        setTopAnchor(pivon, hra.getHerniPlan().getAktualniProstor().getPosX());
+        setLeftAnchor(pivon, hra.getHerniPlan().getAktualniProstor().getPosY());
     }
 }
